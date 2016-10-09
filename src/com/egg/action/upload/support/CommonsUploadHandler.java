d2711751be64e07d2dc3f98ab0384aadcbf2b8bb
egger <625892330@qq.com>
2016-10-09 22:12:43 +0800

    20161009 修改使用CommonFileUpload包上传文件时，获取参数的方法

--- src/com/egg/action/upload/support/CommonsUploadHandler.java
+++ src/com/egg/action/upload/support/CommonsUploadHandler.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -70,7 +73,6 @@ public class CommonsUploadHandler implements UploadHandler {
 			if (!tmpFile.exists()) {
 				tmpFile.mkdirs();
 			}
-
 			DiskFileItemFactory factory = new DiskFileItemFactory();
 			factory.setSizeThreshold(4096);
 			factory.setRepository(tmpFile);
@@ -82,17 +84,33 @@ public class CommonsUploadHandler implements UploadHandler {
 			}
 
 			fileParts = new ArrayList<FilePart>();
-			// TODO <string, list> --> <String, String[]>
-			params = new HashMap<String, String[]>();
+			Map<String, List<String>> paramsListMap = new HashMap<String, List<String>>();
 			FilePart filePart = null;
+			List<String> valueList;
 			for (FileItem fileItem : fileItems) {
 				if (fileItem.isFormField()) {
-					params.put(fileItem.getFieldName(), new String[] { fileItem.getString(UTF8) });
+					if (paramsListMap.containsKey(fileItem.getFieldName())) {
+						valueList = paramsListMap.get(fileItem.getFieldName());
+					} else {
+						valueList = new ArrayList<String>();
+						paramsListMap.put(fileItem.getFieldName(), valueList);
+					}
+					valueList.add(fileItem.getString(UTF8));
 				} else {
 					filePart = this.build(fileItem);
 					if (filePart != null) {
 						fileParts.add(filePart);
 					}
+				}
+			}
+
+			if (!paramsListMap.isEmpty()) {
+				params = new HashMap<String, String[]>(paramsListMap.size());
+				Set<Entry<String, List<String>>> set = paramsListMap.entrySet();
+				List<String> value;
+				for (Entry<String, List<String>> entry : set) {
+					value = entry.getValue();
+					params.put(entry.getKey(), value.toArray(new String[value.size()]));
 				}
 			}
 		} catch (Exception e) {
